71ef49739e0f84bfd668e019a76c189c27897ee3
Mba96 <mbajameel90@gmail.com>
2023-03-22 22:31:12 +0000

    basics of useState, rules of Hooks, and useState with Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
-import Final from './tutorial/01-useState/final/03-useState-array'
-
+import Starter from "./tutorial/01-useState/starter/03-useState-array";
+import Final from "./tutorial/01-useState/final/03-useState-array";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Starter />
-      <Final />
     </div>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,48 @@
+import { data } from "../../../data";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removePerson = (id) => {
+    // console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+
+    /*OR
+    setPeople(people.filter((person) => person.id !== id));
+     */
+  };
+
+  const removeAllPersons = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        // console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type="button" onClick={() => removePerson(id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={removeAllPersons}
+        //OR onClick={() => setPeople([])} with upFunction
+      >
+        clear all
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
