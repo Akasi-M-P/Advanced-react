c96f92b8cfedd4281746c5ffad7f2de81f6b3c08
Mba96 <mbajameel90@gmail.com>
2023-03-27 23:59:01 +0000

    useEffects and Fetching Data

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,56 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users/DavidRoberts";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  // convention to setup booleans with isSomething
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const resp = await fetch(url);
+        const user = await resp.json();
+        // console.log(user);
+        setUser(user);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      // hide loading
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+  // order matters
+  // don't place user JSX before loading or error
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+
+  // Destructure the user object to get the necessary properties
+  const { avatar_url, login, company, bio } = user;
+
+  return (
+    <section>
+      <h2>Fetch Data</h2>
+      <img
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={avatar_url}
+        alt={login}
+      />
+      <div>
+        <h3>{login}</h3>
+        <h4>Works At {company}</h4>
+        <h5>{bio}</h5>
+      </div>
+    </section>
+  );
 };
+
 export default MultipleReturnsFetchData;
